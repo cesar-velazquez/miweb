0c81f23cbcd5326b5996ce283a931954c7c524c7
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-09-05 19:01:46 -0600

    9° commit, update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
 
             <hr className='bg-gray-500 h-[2px] mx-8 ' />
             <h1 className='text-2xl font-urbanist text-center sm:text-4xl font-extrabold my-4 '>Proyectos</h1>
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-12 m-auto max-w-[1200px]'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-12 m-auto max-w-[1200px] pb-16'>
 
                 <div className='relative  w-[250px] h-[200px] flex m-auto py-4'>
                     <div className=' flex flex-col justify-center items-center cursor-pointer hover:opacity-100'>
@@ -287,34 +287,6 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-
-                <div className='relative  w-[250px] h-[200px] flex m-auto py-4'>
-                    <div className=' flex flex-col justify-center items-center cursor-pointer hover:opacity-100'>
-                        <img className='rounded-3xl w-[250px] h-[200px]' src="/imgs/projects/pokedex.PNG" alt="Pokedex" />
-                    </div>
-                    <div className='opacity-0 absolute  hover:opacity-100 hover:top-0 hover:right-0  
-                    hover:border hover:text-white hover:w-[250px] h-[200px] rounded-3xl font-bold
-                    hover:animate-fade-down hover:animate-ease-out
-                    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-400 via-gray-50 to-teal-300
-                    dark:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900'>
-                        <h3 className=' text-center font-urbanist p-2 text-black dark:text-white dark:bg-black/20 dark:rounded-3xl '>Api de pokemon</h3>
-                        <div className='flex justify-center items-center gap-3' >
-                            <i className='bx bxl-javascript text-[20px] text-black dark:text-white'></i>
-                            <i className='bx bxl-tailwind-css text-[20px] text-black dark:text-white'></i>
-                            <i className='bx bxl-react text-[20px] text-black dark:text-white'></i>
-                        </div>
-                        <div className='flex gap-4 justify-center py-4'>
-                            <a href='https://acadpokedex.netlify.app/pokedex' target='_blank'
-                                className='cursor-pointer hover:border border-b-2 border-black w-[50%] m-1 text-center rounded-3xl p-1  hover:bg-black hover:text-white text-black dark:text-white'
-                            >Visitar Sitio</a>
-                            <a href='https://github.com/cesar-velazquez/Pokedex' target='_blank'
-                                className='cursor-pointer hover:border border-b-2 border-black w-[50%] m-1 text-center rounded-3xl hover:p-1 py-2 hover:bg-black hover:text-white text-black dark:text-white'
-                            >Ver Código</a>
-                        </div>
-                    </div>
-                </div>
-
-
 
             </div>
         </main>
